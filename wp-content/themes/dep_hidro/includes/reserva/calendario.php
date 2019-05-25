b690e98ce8eb42e8f6070eac57c84ee8fad96fbd
tito <cdpsk8@gmail.com>
2019-05-24 23:12:15 -0500

    plugin reserva y estructura sass

---
+++ wp-content/themes/dep_hidro/includes/reserva/calendario.php
@@ -0,0 +1,3 @@
+<!-- wp:booking/booking {"wpbc_shortcode":"[booking]"} -->
+<div class="wp-block-booking-booking"><?php Echo do_shortcode (" [booking]"); ?></div>
+<!-- /wp:booking/booking -->
---
+++ wp-content/themes/dep_hidro/templates/_reserva-tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+  Template Name: Reserva Template
+*/
+
+	get_template_part('includes/header');
+
+			get_template_part('includes/reserva/calendario');
+
+	get_template_part('includes/footer');
